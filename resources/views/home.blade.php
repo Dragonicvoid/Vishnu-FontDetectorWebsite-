51ebb14431e69a39ed5448ec497f5bcf094b8b82
Dragonicvoid <44317147+Dragonicvoid@users.noreply.github.com>
2018-10-25 23:26:20 +0700

    even more stuff

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <script type="text/javascript" src="https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/oliver-moran/jimp/v0.2.27/browser/lib/jimp.min.js"></script>
+    <script type="module" src="{{ URL::asset('js/test.js') }}"></script>
+    <title>Test</title>
+</head>
+
+<style>
+    #navbar{
+        border: 1px solid black;
+        text-align: right;
+        right: 0px;
+    }
+
+    #kosong{
+        display: none;
+    }
+</style>
+
+<body>
+    <div id="navbar">
+        Hello, <a href="/table/{{!empty($userName) ? $userName : 'Guest'}}">{{!empty($userName) ? $userName : 'Guest'}}</a>
+    </div>
+
+    <div id="upload-place">
+        <form action="/home/" method="POST" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <img src="images\web.png">
+            <input value="{{empty($userName) ? '' : $userName}}" name="userName" id="kosong">
+            <input type="file" name="uploaded_image" id="uploadImage">
+            <input type="submit" value="Done">
+        </form>
+
+    </div>
+</body>
+</html>
---
+++ resources/views/home2.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <script type="text/javascript" src="https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/oliver-moran/jimp/v0.2.27/browser/lib/jimp.min.js"></script>
+    <script type="module" src="{{ URL::asset('js/test.js') }}"></script>
+    <title>Test</title>
+</head>
+
+<style>
+    #navbar{
+        border: 1px solid black;
+        text-align: right;
+        right: 0px;
+    }
+
+    #kosong{
+        display: none;
+    }
+
+    #table{
+        border : 1px solid black;
+    }
+</style>
+
+<body>
+    <div id="navbar">
+        Hello, <a href="/table/{{!empty($userName) ? $userName : 'Guest'}}">{{!empty($userName) ? $userName : 'Guest'}}</a>
+    </div>
+
+    <div id="upload-place">
+        <table id="table">
+            <tr>
+                <th>Foto</th>
+                <th>Data Hasil</th>
+            </tr>
+            @foreach($images as $i)
+                <tr>
+                    <td>
+                        <img src="/images/{{$i->alamat_gambar}}" width="200px" height="200px">/images/{{$i->alamat_gambar}}
+                    </td>
+                    <td>{{$i->hasil_scan}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</body>
+</html>
